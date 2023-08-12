5b97a686edc1072c1c84197373a0031d29a563bc
Jose Antonio Lopez Gonzalez <tijito4@gmail.com>
2023-08-12 14:48:51 +0200

    Changin the place where toArrayAssoc is implement (from Model to Resource) of Product

--- app/Http/Resources/v1/ProductResource.php
+++ app/Http/Resources/v1/ProductResource.php
@@ -14,6 +14,13 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return $this->toArrayAssoc();
+        return array_merge($this->article->toArrayAssoc() , [
+            'species' => $this->species->toArrayAssoc(),
+            'captureZone' => $this->captureZone->toArrayAssoc(),
+            'articleGtin' => $this->article_gtin,
+            'boxGtin' => $this->box_gtin,
+            'palletGtin' => $this->pallet_gtin,
+            'fixedWeight' => $this->fixed_weight,
+        ]);
     }
 }
