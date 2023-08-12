bd4684ee0165c298809ce46d9aeba57ecac87643
Jose Antonio Lopez Gonzalez <tijito4@gmail.com>
2023-08-12 14:29:03 +0200

    Agregando según documentación Laravel permisos para evitar error CORS v5

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,6 +16,10 @@ class Cors
     public function handle(Request $request, Closure $next): Response
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*');
+        ->header("Access-Control-Allow-Origin", "http://apì.congeladosbrisamar.es")
+        //Métodos que a los que se da acceso
+        ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+        //Headers de la petición
+        ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization"); 
     }
 }
