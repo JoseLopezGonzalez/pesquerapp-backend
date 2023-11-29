bbdee5d3e27c0167470675587683913928f31ebb
Jose-PCBRISAMAR <tijito4@gmail.com>
2023-11-29 17:32:52 +0100

    Añadiendo locateds y unlocateds filter v6

--- app/Http/Controllers/v1/PalletController.php
+++ app/Http/Controllers/v1/PalletController.php
@@ -47,13 +47,13 @@ class PalletController extends Controller
 
         /* Filtros para unlocateds y locateds */
         if ($request->has('unlocateds') && $request->input('unlocateds') == 'on') {
-            $query->whereDoesntHave('storedPallets', function ($subQuery) {
+            $query->whereDoesntHave('stored_pallets', function ($subQuery) {
                 $subQuery->whereNull('position');
             });
         }
 
         if ($request->has('locateds') && $request->input('locateds') == 'on') {
-            $query->whereHas('storedPallets', function ($subQuery) {
+            $query->whereHas('stored_pallets', function ($subQuery) {
                 $subQuery->whereNotNull('position');
             });
         }
