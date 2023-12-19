d217a36b41b113f7e522d093dfe65fcfe5836c66
Jose-PCBRISAMAR <tijito4@gmail.com>
2023-12-19 18:25:49 +0100

    Agregando array de orders personalizado

--- app/Http/Resources/v1/OrderResource.php
+++ app/Http/Resources/v1/OrderResource.php
@@ -14,6 +14,23 @@ class OrderResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'customer' => $this->customer->toArrayAssoc(),
+            'paymentTerm' => $this->payment_term->toArrayAssoc(),
+            'billingAddress' => $this->billing_address,
+            'shippingAddress' => $this->shipping_address,
+            'transportationNotes' => $this->transportation_notes,
+            'productionNotes' => $this->production_notes,
+            'accountingNotes' => $this->accounting_notes,
+            'salesperson' => $this->salesperson->toArrayAssoc(),
+            'emails' => $this->emails,
+            'transport' => $this->transport->toArrayAssoc(),
+            'entryDate' => $this->entry_date,
+            'loadDate' => $this->load_date,
+            'status' => $this->status, 
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+        ];
     }
 }
