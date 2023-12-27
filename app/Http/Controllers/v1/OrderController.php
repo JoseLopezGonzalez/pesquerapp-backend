28ab530fd1b96483057d8e018b41b2947e626f0f
Jose-PCBRISAMAR <tijito4@gmail.com>
2023-12-27 16:43:21 +0100

    Añadiendo Active Orders v3

--- app/Http/Controllers/v1/OrderController.php
+++ app/Http/Controllers/v1/OrderController.php
@@ -17,10 +17,10 @@ class OrderController extends Controller
     {
         if($request->has('active')){
             if($request->active == 'true'){
-                return OrderResource::collection(Order::where('status', '!=', 'finished')->orWhere('load_date', '<', now())->get());
+                return OrderResource::collection(Order::where('status', '!=', 'finished')->orWhere('load_date', '>=', now())->get());
                 /* sustituir en un futuro por comparar el atributo activo */
             }else{
-                return OrderResource::collection(Order::where('status', 'finished')->where('load_date', '>', now())->get());
+                return OrderResource::collection(Order::where('status', 'finished')->where('load_date', '<', now())->get());
                 
             }
         }else{
