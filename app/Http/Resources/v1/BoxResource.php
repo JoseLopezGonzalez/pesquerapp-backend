00da12a5d9a9101c7308a08e9c1592e8c518387e
Jose Antonio Lopez Gonzalez <tijito4@gmail.com>
2024-03-10 20:17:21 +0100

    Corrigiendo errores v22

--- app/Http/Resources/v1/BoxResource.php
+++ app/Http/Resources/v1/BoxResource.php
@@ -16,7 +16,7 @@ class BoxResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'palletId' => $this->pallet->id ,
+            'palletId' => $this->pallet ? $this->pallet->id : null,
             'article' => $this->article->toArrayAssoc(),
             'lot' => $this->lot,
             'gs1128' => $this->gs1_128,
