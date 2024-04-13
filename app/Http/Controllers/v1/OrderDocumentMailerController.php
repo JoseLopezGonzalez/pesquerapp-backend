1056385937985aeb2e87ed79b62cc8c508b11adf
Jose Antonio Lopez Gonzalez <tijito4@gmail.com>
2024-04-13 11:45:59 +0200

    Añadiendo envio de email (ORDER) v3

--- app/Http/Controllers/v1/OrderDocumentMailerController.php
+++ app/Http/Controllers/v1/OrderDocumentMailerController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\v1;
 
+use App\Http\Controllers\Controller;
 use App\Mail\OrderShipped;
 use App\Models\Order;
 use Illuminate\Http\Request;
