394061845abb6b75f079791336aa9079cba26b32
Jose Antonio Lopez Gonzalez <tijito4@gmail.com>
2024-04-13 12:32:37 +0200

    Customizando Contenido del Correo v8

--- resources/views/emails/orders/shipped.blade.php
+++ resources/views/emails/orders/shipped.blade.php
@@ -1,31 +1,31 @@
 <x-mail::message>
 # {{ $customer_name }}
 
-### ES
 
-Su pedido con número **#{{$order_id}}** ha sido enviado. En breve recibirá su factura.
 
-### IT
+**ES -** Su pedido con número **#{{$order_id}}** ha sido enviado. En breve recibirá su factura.
 
-Il suo ordine con il numero **#{{$order_id}}** è stato spedito. Riceverà presto la sua fattura.
 
-### EN
 
-Your order with number **#{{$order_id}}** has been shipped. You will receive your invoice shortly.
+**IT -** Il suo ordine con il numero **#{{$order_id}}** è stato spedito. Riceverà presto la sua fattura.
 
-### FR
 
-Votre commande avec le numéro **#{{$order_id}}** a été expédiée. Vous recevrez bientôt votre facture.
 
-### PT
+**EN -** Your order with number **#{{$order_id}}** has been shipped. You will receive your invoice shortly.
 
-Seu pedido com o número **#{{$order_id}}** foi enviado. Você receberá sua fatura em breve.
+
+
+**FR -** Votre commande avec le numéro **#{{$order_id}}** a été expédiée. Vous recevrez bientôt votre facture.
+
+
+
+**PT -** Seu pedido com o número **#{{$order_id}}** foi enviado. Você receberá sua fatura em breve.
 
 
 <x-mail::button :url="''">
-Button Texte
+Delivery Note
 </x-mail::button>
 
-Thanks,<br>
+Saludos / Saluti / Best regards / Cordialement / Atenciosamente,<br>
 {{ config('app.name') }}
 </x-mail::message>
