b8fb48d7d80a0c5c4a5073090d8cc273c6de0e65
Jose Antonio Lopez Gonzalez <tijito4@gmail.com>
2024-04-13 12:05:12 +0200

    Customizando Contenido del Correo v3

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+<img src="https://app.congeladosbrisamar.es/logo-icono-letras-horizontal.svg" class="logo" alt="Brisamar Logo">
 @else
 {{ $slot }}
 @endif
