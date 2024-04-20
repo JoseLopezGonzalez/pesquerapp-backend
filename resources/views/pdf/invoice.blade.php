9218f5bf7cc89745f94e9c80c1d5396546fe9605
Jose Antonio Lopez Gonzalez <tijito4@gmail.com>
2024-04-20 17:42:07 +0200

    Modificando albaranes v30

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -1,14 +1,1 @@
-<!-- resources/views/pdf/delivery_note.blade.php -->
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Delivery Note </title>
-  
-</head>
-<body>
-    <div class="pt-4">
-      hola
-    </div>
-    
-</body>
-</html>
+h
