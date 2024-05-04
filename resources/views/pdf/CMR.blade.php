d77b2cbbc85e96b9f54ab9ab823a873b49f390c5
Jose Antonio Lopez Gonzalez <tijito4@gmail.com>
2024-05-04 13:30:09 +0200

    Añadiendo CMR

--- resources/views/pdf/CMR.blade.php
+++ resources/views/pdf/CMR.blade.php
@@ -35,9 +35,7 @@
             <div class="imprimir" style="width: 21cm; height: 29.7cm; position: relative; page-break-before: always;">
                 <div class="text-center text-uppercase" style="position: relative;">
                     <div style="position: absolute;">
-                        <img src="{{ asset('images/documents/CMR/{{ $img }}') }}" width="100%" />
-                        {{-- <img src="images/documents/CMR/cmr-page-1.png" width="100%" /> --}}{{-- {{ $img }} --}}
-                {{-- <img src="{{ asset(env('DELIVERY_NOTE_LOGO_PATH')) }}" class="h-24" alt="Logo" > --}}
+                        <img src="{{ asset('images/documents/CMR/' . $img) }}" width="100%" />
 
                     </div>
                     <p style="text-align: left; font-size: 9pt; left: 65px; top: 80px; position: absolute;">
