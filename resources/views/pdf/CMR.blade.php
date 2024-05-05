fbb13ed5a8de727457ff2a609251433fc47006a0
Jose Antonio Lopez Gonzalez <tijito4@gmail.com>
2024-05-05 14:17:05 +0200

    Añadiendo CMR

--- resources/views/pdf/CMR.blade.php
+++ resources/views/pdf/CMR.blade.php
@@ -39,7 +39,7 @@
         @foreach ($imgs as $index => $img)
             <div class="imprimir  h-screen " >
                 <div class="text-center text-uppercase h-full" style="position: relative;">
-                    <div style="position: absolute; h-full bg-red-500">
+                    <div style="position: absolute; h-full bg-red-500 overflow-hidden">
                         <img src="{{ asset('images/documents/CMR/' . $img) }}" class="h-full" />
 
                     </div>
