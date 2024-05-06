9ad128571c408dcf5339d458461fd88cb1e6ac88
Jose-PCBRISAMAR <tijito4@gmail.com>
2024-05-06 17:23:21 +0200

    MOD EMAIL

--- app/Mail/OrderShipped.php
+++ app/Mail/OrderShipped.php
@@ -72,16 +72,53 @@ class OrderShipped extends Mailable
                     ]); */
 
 
-        $snappdf = new Snappdf(); 
+        $snappdf = new Snappdf();
         $html = view('pdf.delivery_note', ['order' => $this->order])->render();
         $snappdf->setChromiumPath('/usr/bin/google-chrome'); // Configura el camino correcto a Chrome
-        $snappdf->setHtml($html);
 
         // Personaliza tu PDF con argumentos y opciones
         $snappdf->addChromiumArguments('--no-sandbox');
         // Añadir más argumentos según necesidad
 
-        $pdfContent = $snappdf->generate();
+        /* NUEVO */
+
+        $snappdf->setChromiumPath('/usr/bin/google-chrome'); // Asegúrate de cambiar esto por tu ruta específica
+
+        /* Personalizando el PDF */
+        $snappdf->addChromiumArguments('--margin-top=10mm');
+        $snappdf->addChromiumArguments('--margin-right=30mm');
+        $snappdf->addChromiumArguments('--margin-bottom=10mm');
+        $snappdf->addChromiumArguments('--margin-left=10mm');
+
+
+        // Agrega argumentos de Chromium uno por uno
+        // Configuración para que el servidor no de errores y pueda trabajar bien con el PDF
+        $snappdf->addChromiumArguments('--no-sandbox');
+        $snappdf->addChromiumArguments('disable-gpu');
+        $snappdf->addChromiumArguments('disable-translate');
+        $snappdf->addChromiumArguments('disable-extensions');
+        $snappdf->addChromiumArguments('disable-sync');
+        $snappdf->addChromiumArguments('disable-background-networking');
+        $snappdf->addChromiumArguments('disable-software-rasterizer');
+        $snappdf->addChromiumArguments('disable-default-apps');
+        $snappdf->addChromiumArguments('disable-dev-shm-usage');
+        $snappdf->addChromiumArguments('safebrowsing-disable-auto-update');
+        $snappdf->addChromiumArguments('run-all-compositor-stages-before-draw');
+        $snappdf->addChromiumArguments('no-first-run');
+        $snappdf->addChromiumArguments('no-margins');
+        $snappdf->addChromiumArguments('print-to-pdf-no-header');
+        $snappdf->addChromiumArguments('no-pdf-header-footer');
+        $snappdf->addChromiumArguments('hide-scrollbars');
+        $snappdf->addChromiumArguments('ignore-certificate-errors');
+
+        $pdfContent = $snappdf->setHtml($html)
+        ->generate();
+
+        /* NUEVO */
+
+
+
+        /* $pdfContent = $snappdf->generate(); */
 
         // Guarda temporalmente el PDF para adjuntarlo
         $pdfPath = storage_path('app/public/delivery-note-' . $this->order->id . '.pdf');
