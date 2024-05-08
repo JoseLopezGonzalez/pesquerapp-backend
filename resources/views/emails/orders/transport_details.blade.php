21b84e01029b9e54176f1ff7ee7426292c23eac6
Jose-PCBRISAMAR <tijito4@gmail.com>
2024-05-08 14:18:17 +0200

    Añadiendo email al transporte

--- resources/views/emails/orders/transport_details.blade.php
+++ resources/views/emails/orders/transport_details.blade.php
@@ -19,7 +19,7 @@
 <br>
 Por favor, revise los documentos adjuntos para asegurarse de que todos los detalles son correctos y que tiene todo lo necesario para un proceso de transporte sin inconvenientes.
 
-
+<br>
 *Si encuentra alguna discrepancia o si necesita más información, no dude en contactarnos al +34 620 714 139.*
 
 
