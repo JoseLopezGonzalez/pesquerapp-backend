331693a5b05145649d447c8f25c07b71d5a2efbb
Jose-PCBRISAMAR <tijito4@gmail.com>
2024-05-08 18:10:52 +0200

    Añadiendo email al transporte

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
+{{-- <a href="{{ $url }}" style="display: inline-block;"> --}}
 @if (trim($slot) === 'Congelados_Brisamar_App')
 <img src="https://congeladosbrisamar.es/logos/logo-brisamar-small.png" class="logo" alt="Brisamar Logo">
 @else
