7daa73e22dc31ac37ce1aec2cef409eab8cdf8d5
Jose-PCBRISAMAR <tijito4@gmail.com>
2024-05-08 17:35:55 +0200

    Añadiendo email al transporte

--- resources/views/emails/orders/transport_details.blade.php
+++ resources/views/emails/orders/transport_details.blade.php
@@ -20,15 +20,6 @@
 </x-mail::table>
 
 
-
-
-
-| Laravel       | Table         | Example  |
-| ------------- | ------------- | -------- |
-| Col 2 is      | Centered      | $10      |
-| Col 3 is      | Right-Aligned | $20      |
-
-
 ## Documentación Adjunta:
 
 <x-mail::panel>
