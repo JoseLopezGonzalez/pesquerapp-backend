763e4431ff498a208c81fa535cc2db05501fd487
Jose-PCBRISAMAR <tijito4@gmail.com>
2024-05-08 18:09:21 +0200

    Añadiendo email al transporte

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -1,9 +1,9 @@
 <x-mail::layout>
 {{-- Header --}}
 <x-slot:header>
-{{-- <x-mail::header :url="config('app.url')">
-{{ config('app.name') }}
-</x-mail::header> --}}
+<x-mail::header {{-- :url="config('app.url')" --}}>
+{{-- {{ config('app.name') }} --}}
+</x-mail::header>
 </x-slot:header>
 
 {{-- Body --}}
