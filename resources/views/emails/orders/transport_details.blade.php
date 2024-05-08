0d5f97b2406ebe68800c46fc9223919f75424603
Jose-PCBRISAMAR <tijito4@gmail.com>
2024-05-08 13:34:52 +0200

    Añadiendo email al transporte

--- resources/views/emails/orders/transport_details.blade.php
+++ resources/views/emails/orders/transport_details.blade.php
@@ -26,9 +26,6 @@
 
 Se adjunta la documentación relevante necesaria para la manipulación y transporte de las mercancías. Por favor, revise los documentos adjuntos para asegurarse de que todos los detalles son correctos y que tiene todo lo necesario para un proceso de transporte sin inconvenientes.
 
-@component('mail::button', ['url' => $urlToDocumentation, 'color' => 'success'])
-Ver Documentación
-@endcomponent
 
 Si encuentra alguna discrepancia o si necesita más información, no dude en contactarnos al +34 620 714 139.
 
