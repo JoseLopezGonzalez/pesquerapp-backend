d85e82b2848dd01baa05a8b5716a75fb2dd15144
Jose-PCBRISAMAR <tijito4@gmail.com>
2024-05-31 11:25:43 +0200

    Actualizando

--- app/Http/Controllers/v1/RawMaterialReceptionController.php
+++ app/Http/Controllers/v1/RawMaterialReceptionController.php
@@ -25,7 +25,7 @@ class RawMaterialReceptionController extends Controller
             'date' => 'required|date',
             'notes' => 'nullable|string',
             'details' => 'required|array',
-            'details.*.product.id' => 'required|exists:details,id',
+            'details.*.product.id' => 'required|exists:products,id',
             'details.*.netWeight' => 'required|numeric',
         ]);
 
