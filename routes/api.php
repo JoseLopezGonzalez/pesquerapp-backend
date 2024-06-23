783b5612a14da854b1223cb476e4561a9393b400
Jose Antonio Lopez Gonzalez <tijito4@gmail.com>
2024-06-23 23:55:02 +0200

    Actualizando

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\v1\AuthController;
 use App\Http\Controllers\v1\OrderDocumentMailerController;
 use App\Http\Controllers\v1\BoxesReportController;
 use App\Http\Controllers\v1\CustomerController;
@@ -48,55 +49,48 @@ use Illuminate\Support\Facades\App;
     Route::apiResource('v1/articles/products', ProductController::class)->only(['show' , 'index']);
 }); */
 
-Route::apiResource('v1/stores/pallets', StoredPalletController::class)
-    ->names([
-        'index' => 'stores.pallets.index',
-        'create' => 'stores.pallets.create',
-        'store' => 'stores.pallets.store',
-        'show' => 'stores.pallets.show',
-        'edit' => 'stores.pallets.edit',
-        'update' => 'stores.pallets.update',
-        'destroy' => 'stores.pallets.destroy',
-    ]);
-Route::apiResource('v1/pallets', PalletController::class);
-Route::apiResource('v1/stores', StoreController::class)->only(['show', 'index']);
-Route::apiResource('v1/articles/products', ProductController::class)->only(['show', 'index']);
-Route::apiResource('v1/customers', CustomerController::class);
-Route::apiResource('v1/orders', OrderController::class);
-Route::apiResource('v1/transports', TransportController::class);
-Route::apiResource('v1/salespeople', SalespersonController::class);
-Route::apiResource('v1/payment_terms', PaymentTermController::class);
-Route::apiResource('v1/productions', ProductionController::class);
-Route::apiResource('v1/suppliers', SupplierController::class);
-Route::apiResource('v1/raw-material-receptions', RawMaterialReceptionController::class);
-Route::apiResource('v1/species', SpeciesController::class);
-Route::apiResource('v1/raw-materials', RawMaterialController::class);
+Route::post('v1/register', [AuthController::class, 'register']);
+Route::post('v1/login', [AuthController::class, 'login']);
+Route::post('v1/logout', [AuthController::class, 'logout']);
+Route::get('v1/me', [AuthController::class, 'me'])->middleware('auth:api');
 
 
+//Route::group(['middleware' => ['auth:api']], function () {
 
+    Route::apiResource('v1/stores/pallets', StoredPalletController::class)
+        ->names([
+            'index' => 'stores.pallets.index',
+            'create' => 'stores.pallets.create',
+            'store' => 'stores.pallets.store',
+            'show' => 'stores.pallets.show',
+            'edit' => 'stores.pallets.edit',
+            'update' => 'stores.pallets.update',
+            'destroy' => 'stores.pallets.destroy',
+        ]);
+    Route::apiResource('v1/pallets', PalletController::class);
+    Route::apiResource('v1/stores', StoreController::class)->only(['show', 'index']);
+    Route::apiResource('v1/articles/products', ProductController::class)->only(['show', 'index']);
+    Route::apiResource('v1/customers', CustomerController::class);
+    Route::apiResource('v1/orders', OrderController::class);
+    Route::apiResource('v1/transports', TransportController::class);
+    Route::apiResource('v1/salespeople', SalespersonController::class);
+    Route::apiResource('v1/payment_terms', PaymentTermController::class);
+    Route::apiResource('v1/productions', ProductionController::class);
+    Route::apiResource('v1/suppliers', SupplierController::class);
+    Route::apiResource('v1/raw-material-receptions', RawMaterialReceptionController::class);
+    Route::apiResource('v1/species', SpeciesController::class);
+    Route::apiResource('v1/raw-materials', RawMaterialController::class);
+    /* Incorterm */
+    Route::apiResource('v1/incoterms', IncotermController::class);
+    Route::get('v1/boxes_report', [BoxesReportController::class, 'exportToExcel'])->name('export.boxes');
+    // Ruta personalizada para enviar documentación de un pedido (NO CRUD)
+    Route::post('v1/send_order_documentation/{orderId}', [OrderDocumentMailerController::class, 'sendDocumentation'])->name('send_order_documentation');
+    /* Send order documentation to Transport  */
+    Route::post('v1/send_order_documentation_transport/{orderId}', [OrderDocumentMailerController::class, 'sendDocumentationTransport'])->name('send_order_documentation_transport');
+    Route::get('v1/orders/{orderId}/delivery-note', [PDFController::class, 'generateDeliveryNote'])->name('generate_delivery_note');
+    Route::get('v1/orders/{orderId}/restricted-delivery-note', [PDFController::class, 'generateRestrictedDeliveryNote'])->name('generate_restricted_delivery_note');
+    Route::get('v1/orders/{orderId}/order-signs', [PDFController::class, 'generateOrderSigns'])->name('generate_order_signs');
+    Route::get('v1/orders/{orderId}/order_CMR', [PDFController::class, 'generateOrderCMR'])->name('generate_order_CMR');
+    Route::get('v1/rawMaterialReceptions/document', [PDFController::class, 'generateRawMaterialReceptionsDocument'])->name('generate_raw_material_receptions_document');
 
-/* Incorterm */
-Route::apiResource('v1/incoterms', IncotermController::class);
-/* Route::apiResource('v1/boxes_report', BoxesReportController::class)->only(['index']); */
-Route::get('v1/boxes_report', [BoxesReportController::class, 'exportToExcel'])->name('export.boxes');
-
-
-
-
-// Ruta personalizada para enviar documentación de un pedido (NO CRUD)
-Route::post('v1/send_order_documentation/{orderId}', [OrderDocumentMailerController::class, 'sendDocumentation'])->name('send_order_documentation');
-/* Send order documentation to Transport  */
-Route::post('v1/send_order_documentation_transport/{orderId}', [OrderDocumentMailerController::class, 'sendDocumentationTransport'])->name('send_order_documentation_transport');
-Route::get('v1/orders/{orderId}/delivery-note', [PDFController::class, 'generateDeliveryNote'])->name('generate_delivery_note');
-Route::get('v1/orders/{orderId}/restricted-delivery-note', [PDFController::class, 'generateRestrictedDeliveryNote'])->name('generate_restricted_delivery_note');
-Route::get('v1/orders/{orderId}/order-signs', [PDFController::class, 'generateOrderSigns'])->name('generate_order_signs');
-Route::get('v1/orders/{orderId}/order_CMR', [PDFController::class, 'generateOrderCMR'])->name('generate_order_CMR');
-Route::get('v1/rawMaterialReceptions/document', [PDFController::class, 'generateRawMaterialReceptionsDocument'])->name('generate_raw_material_receptions_document');
-
-
-
-Route::get('/test-pdf', function () {
-    $pdf = App::make('snappy.pdf.wrapper');
-    $pdf->loadHTML('<h1>Test PDF</h1>');
-    return $pdf->inline();
-});
+//});
