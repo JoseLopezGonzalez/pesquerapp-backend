2de9a5baf8e63e4baaf9072ab67e6af0523100a1
Jose-PCBRISAMAR <tijito4@gmail.com>
2024-09-04 17:01:27 +0200

    Optimizando codigo

---
+++ app/Exports/v1/RawMaterialReceptionExport.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace App\Exports\v1;
+
+use App\Models\Box;
+use App\Models\RawMaterialReception;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\Exportable;
+
+class RawMaterialReceptionExport implements FromQuery, WithHeadings, WithMapping
+{
+    use Exportable;
+
+    protected $filters;
+
+    public function __construct(Request $this->filters->input)
+    {
+        $this->filters = $this->filters->input;  // Almacenar la solicitud completa puede no ser lo ideal. Es mejor pasar solo los filtros necesarios.
+    }
+
+    public function query()
+    {
+        /* $query = Box::query();
+
+        if ($this->filters->has('id')) {
+            $id = $this->filters->input('id');
+            $query->whereHas('palletBox', function ($subQuery) use ($id) {
+                $subQuery->where('pallet_id', 'like', "%{$id}%");
+            });
+        }
+
+        if ($this->filters->has('state')) {
+            if ($this->filters->input('state') == 'stored') {
+                $query->whereHas('palletBox.pallet', function ($subQuery) {
+                    $subQuery->where('state_id', 2);
+                });
+            } else if ($this->filters->input('state') == 'shipped') {
+                $query->whereHas('palletBox.pallet', function ($subQuery) {
+                    $subQuery->where('state_id', 3);
+                });
+            }
+        }
+
+        if ($this->filters->has('position')) {
+            if ($this->filters->input('position') == 'located') {
+                $query->whereHas('palletBox.pallet', function ($subQuery) {
+                    $subQuery->whereHas('storedPallet', function ($subSubQuery) {
+                        $subSubQuery->whereNotNull('position');
+                    });
+                });
+            } else if ($this->filters->input('position') == 'unlocated') {
+                $query->whereHas('palletBox.pallet', function ($subQuery) {
+                    $subQuery->whereHas('storedPallet', function ($subSubQuery) {
+                        $subSubQuery->whereNull('position');
+                    });
+                });
+            }
+        }
+
+        if ($this->filters->has('dates')) {
+            $dates = $this->filters->input('dates');
+            if (isset($dates['start'])) {
+                $startDate = $dates['start'];
+                $startDate = date('Y-m-d 00:00:00', strtotime($startDate));
+                $query->where('created_at', '>=', $startDate);
+            }
+            if (isset($dates['end'])) {
+                $endDate = $dates['end'];
+                $endDate = date('Y-m-d 23:59:59', strtotime($endDate));
+                $query->where('created_at', '<=', $endDate);
+            }
+        }
+
+        if ($this->filters->has('notes')) {
+            $notes = $this->filters->input('notes');
+            $query->whereHas('palletBox.pallet', function ($subQuery) use ($notes) {
+                $subQuery->where('observations', 'like', "%{$notes}%");
+            });
+        }
+
+        if ($this->filters->has('lots')) {
+            $lots = $this->filters->input('lots');
+            $query->whereIn('lot', $lots);
+        }
+
+        if ($this->filters->has('products')) {
+            $articles = $this->filters->input('products');
+            $query->whereIn('article_id', $articles);
+        } */
+
+        $query = RawMaterialReception::query();
+        $query->with('supplier', 'products.product');
+
+        if ($this->filters->input->has('id')) {
+            $query->where('id', $this->filters->input->id);
+        }
+
+        if ($this->filters->input->has('suppliers')) {
+            $query->whereIn('supplier_id', $this->filters->input->suppliers);
+        }
+
+        if ($this->filters->input->has('dates')) {
+            $query->whereBetween('date', [$this->filters->input->dates['start'], $this->filters->input->dates['end']]);
+        }
+
+        if ($this->filters->input->has('species')) {
+            $species= $this->filters->input->species;
+            $query->whereHas('products.product', function ($query) use ($species) {
+                $query->whereIn('species_id', $this->filters->input->species);
+            });
+        }
+
+        if ($this->filters->input->has('products')) {
+            $products = $this->filters->input->products;
+            $query->whereHas('products.product', function ($query) use ($products) {
+                $query->whereIn('id', $this->filters->input->products);
+            });
+        }
+
+        if ($this->filters->input->has('notes')) {
+            $query->where('notes', 'like', '%' . $this->filters->input->notes . '%');
+        }
+
+        /* Order by Date Descen */
+        $query->orderBy('date', 'desc');
+
+        /* Hay que sacar Para cada linea (RawMaterialReception) de cada RawMaterialReception para ver los siguientes campos:
+            - id : se saca de un indice en un bucle por cada RawMaterialReception (empezando por el 1)
+            - date
+            - articleId : products.product.article.id
+            - articleName : products.product.article.name
+            - netWeight : products.netWeight
+        */
+
+        $rawMaterialReceptionProducts = [];
+        /* el id sale del índice en el primer foreach */
+        foreach ($query as $index => $rawMaterialReception) {
+            foreach ($rawMaterialReception->products as $product) {
+                $rawMaterialReceptionProducts[] = [
+                    'index' => $index, // Guardamos el índice del primer foreach
+                    'id' => $rawMaterialReception->id,
+                    'date' => $rawMaterialReception->date,
+                    'articleId' => $product->product->article->id,
+                    'articleName' => $product->product->article->name,
+                    'netWeight' => $product->netWeight,
+                ];
+            }
+        }
+
+        return $rawMaterialReceptionProducts;
+
+        /* return $query; */
+    }
+    
+    public function headings(): array
+    {
+        return [
+            'CODIGO', 
+            'Fecha', 
+            'Cod.  Producto', 
+            'Producto', 
+            'Cantidad Kg',
+        ];
+    }
+
+    public function map($rawMaterialReceptionProducts): array
+    {
+        return [
+            $rawMaterialReceptionProducts['id'],
+            $rawMaterialReceptionProducts['date'],
+            $rawMaterialReceptionProducts['articleId'],
+            $rawMaterialReceptionProducts['articleName'],
+            $rawMaterialReceptionProducts['netWeight'],
+
+            
+            /* $box->id,
+            $box->article->article->name,  // Suponiendo que Box tiene una relación con Article y que article tiene un atributo 'name'
+            $box->lot,
+            $box->net_weight,
+            $box->gross_weight,
+            $box->created_at, */
+            // Agrega aquí los atributos que desees exportar
+        ];
+    }
+}
---
+++ app/Http/Controllers/v1/RawMaterialReceptionsReportController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Exports\v1\BoxesExport;
+use App\Exports\v1\RawMaterialReceptionExport;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\v1\BoxResource;
+use App\Http\Resources\v1\PalletResource;
+use App\Models\Box;
+use App\Models\Order;
+use App\Models\OrderPallet;
+use App\Models\Pallet;
+use App\Models\PalletBox;
+use App\Models\StoredPallet;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Maatwebsite\Excel\Facades\Excel;
+
+class RawMaterialReceptionsReportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    /*return response()->json(['message' => 'Hola Mundo'], 200);*/
+    /* return PalletResource::collection(Pallet::all()); */
+
+    /*  public function index()
+    {
+        return PalletResource::collection(Pallet::paginate(10));
+
+    } */
+
+    
+
+    public function exportToExcel(Request $request)
+    {
+        ini_set('memory_limit', '1024M');  // Aumentar el límite de memoria solo para esta operación
+        ini_set('max_execution_time', 300);  // Aumentar el tiempo de ejecución solo para esta operación
+        return Excel::download(new RawMaterialReceptionExport($request), 'raw_material_receptions_report.xlsx');
+
+    }
+}
