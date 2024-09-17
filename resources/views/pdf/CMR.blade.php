3659af7eab2b7fd26b69515b4b71587024e31493
Jose-PCBRISAMAR <tijito4@gmail.com>
2024-09-17 17:03:40 +0200

    Agregando tipo de CMR a PDF

--- resources/views/pdf/CMR.blade.php
+++ resources/views/pdf/CMR.blade.php
@@ -77,7 +77,7 @@
                     {{-- img --}}
 
                     <img src="{{ asset('images/documents/CMR/sello-brisamar.png') }}"
-                        style="position: absolute; left: 100px; top: 935px; width: 170px; " />
+                        style="position: absolute; left: 100px; top: 920px; width: 170px; " />
 
 
 
