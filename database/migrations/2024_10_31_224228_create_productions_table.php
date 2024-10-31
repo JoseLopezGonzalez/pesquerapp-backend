95f12e34087a1c225f349cdd0aa78acd982a4ba9
PC-Jose-Home-Ubuntu <tijito4@gmail.com>
2024-11-01 00:03:49 +0100

    Eliminando models inecesarios

--- database/migrations/2024_10_31_224228_create_productions_table.php
+++ database/migrations/2024_10_31_224228_create_productions_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->date('date')->nullable(); // Fecha
             $table->foreignId('species_id')->constrained('species')->onDelete('cascade'); // Clave foránea a species
             $table->foreignId('capture_zone_id')->constrained('capture_zones')->onDelete('cascade'); // Clave foránea a capture_zones
+            $table->text('notes')->nullable(); // Campo de notas
             $table->json('diagram_data'); // JSON del diagrama
             $table->timestamps(); // Marcas de tiempo
         });
