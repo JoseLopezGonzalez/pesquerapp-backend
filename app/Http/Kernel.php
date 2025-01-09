2f2496bee95b3cda612e75ea5d20ee7f8dabf989
PC-Jose-Home-Ubuntu <tijito4@gmail.com>
2025-01-09 16:32:41 +0100

    Actualizando

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'throttle:api',
             \Illuminate\Session\Middleware\StartSession::class, // Habilitar manejo de sesiones
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
