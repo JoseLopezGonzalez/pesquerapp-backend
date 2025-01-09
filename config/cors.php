e4f52504ab2b8fa4473cce11ab96613df899d2f2
PC-Jose-Home-Ubuntu <tijito4@gmail.com>
2025-01-09 17:42:26 +0100

    Actualizando

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*','http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://brisapp-nextjs.vercel.app'],
+
 
     'allowed_origins_patterns' => [],
 
