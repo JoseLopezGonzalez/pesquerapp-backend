a3d970efa7292e661c9210549b513643178553d4
PC-Jose-Home-Ubuntu <tijito4@gmail.com>
2025-01-12 23:27:55 +0100

    Actualizando

---
+++ app/Http/Resources/v2/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\v2;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'roles' => $this->roles->pluck('name'), // Retorna los nombres de los roles
+            'created_at' => $this->created_at ? $this->created_at->format('Y-m-d H:i:s') : null,
+            'updated_at' => $this->updated_at ? $this->updated_at->format('Y-m-d H:i:s') : null,
+        ];
+    }
+}
