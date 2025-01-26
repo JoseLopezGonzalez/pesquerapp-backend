c6600e559063065268bc98e7bc0302ad50661b11
PC-Jose-Home-Ubuntu <tijito4@gmail.com>
2025-01-26 21:41:23 +0100

    Actualizando

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,8 +10,11 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo($request)
     {
-        return $request->expectsJson() ? null : route('login');
+        // Devuelve un error JSON en lugar de redirigir
+        if (!$request->expectsJson()) {
+            return response()->json(['message' => 'No autenticado.'], 401);
+        }
     }
 }
