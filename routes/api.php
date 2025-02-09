37b07fe375803db14b617d50a61159824baf4d8f
PC-Jose-Home-Ubuntu <tijito4@gmail.com>
2025-02-09 21:02:26 +0100

    Actualizando

--- routes/api.php
+++ routes/api.php
@@ -51,6 +51,7 @@ use App\Http\Controllers\v2\PalletController as V2PalletController;
 use App\Http\Controllers\v2\PaymentTermController as V2PaymentTermController;
 use App\Http\Controllers\v2\ProductController as V2ProductController;
 use App\Http\Controllers\v2\RawMaterialReceptionController as V2RawMaterialReceptionController;
+use App\Http\Controllers\v2\RoleController;
 use App\Http\Controllers\v2\SalespersonController as V2SalespersonController;
 use App\Http\Controllers\v2\SessionController;
 use App\Http\Controllers\v2\SpeciesController as V2SpeciesController;
@@ -214,7 +215,10 @@ Route::group(['prefix' => 'v2'], function () {
             Route::apiResource('users', UserController::class);
             /* Activity logs */
             Route::apiResource('activity-logs', ActivityLogController::class);
-            /* user options */
+            /* roles options */
+            Route::get('roles/options', [RoleController::class, 'rolesOptions']);
+            /* roles */
+            Route::apiResource('roles', RoleController::class);
         });
 
         // Rutas para Gerencia
@@ -290,19 +294,14 @@ Route::group(['prefix' => 'v2'], function () {
             Route::apiResource('cebo-dispatches', V2CeboDispatchController::class);
             /* sessions */
 
-                  
-
-
-
-
-
-
 
 
 
         });
     });
 });
+
+
 
 
 
