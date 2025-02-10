326cf68f9907a18c82af44184214f3a9eeaec132
PC-Jose-Home-Ubuntu <tijito4@gmail.com>
2025-02-10 23:23:43 +0100

    Actualizando

--- config/cors.php
+++ config/cors.php
@@ -23,8 +23,8 @@ return [
 
     // Permitir varios orígenes (especifica los dominios)
     'allowed_origins' => [
-        'http://localhost:3000/', // Origen local (por ejemplo, frontend en desarrollo)
-        'https://*.congeladosbrisamar.es/', // Origen de producción
+        'http://localhost:3000', // Origen local (por ejemplo, frontend en desarrollo)
+        'https://*.congeladosbrisamar.es', // Origen de producción
     ],
 
 
