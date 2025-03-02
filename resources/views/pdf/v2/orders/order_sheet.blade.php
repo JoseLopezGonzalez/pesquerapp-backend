919ec27dd9be4cdb9630ff984ff8873fb4593b9b
Jose Laptop Unir <tijito4@gmail.com>
2025-03-02 13:43:20 +0100

    Actualizando

--- resources/views/pdf/v2/orders/order_sheet.blade.php
+++ resources/views/pdf/v2/orders/order_sheet.blade.php
@@ -4,6 +4,8 @@
 
 <head>
     <title>Hoja de Pedido</title>
+    {{-- Tailwind no funciona, lo cojo todo directamente de un cdn --}}
+
     <script src="https://cdn.tailwindcss.com"></script>
 
     <style>
@@ -17,30 +19,32 @@
 
         @page {
             size: A4 portrait;
+            /*  margin: 20mm; */
         }
     </style>
+    {{-- getProductsWithLotsDetailsBySpeciesAndCaptureZoneAttribute --}}
 </head>
 
 <body class="h-full">
-    <div class="flex flex-col max-w-[210mm] mx-auto p-6 bg-white rounded text-black text-xs min-h-screen">
 
-        <!-- ENCABEZADO -->
-        <div class="flex justify-between items-end mb-6">
+    <div class="flex flex-col max-w-[210mm]  mx-auto p-6 bg-white rounded text-black text-xs min-h-screen ">
+        <div class="flex justify-between items-end mb-6 ">
             <div class="flex items-center gap-2">
                 <div>
                     <h1 class="text-md font-bold">Congelados Brisamar S.L.</h1>
-                    <p class="">C/Dieciocho de Julio de 1922 Nº2 - 21410 Isla Cristina</p>
-                    <p class="">Tel: +34 613 09 14 94</p>
-                    <p class="">administracion@congeladosbrisamar.es</p>
+                    <p class=" ">C/Dieciocho de Julio de 1922 Nº2 - 21410 Isla Cristina</p>
+                    <p class=" ">Tel: +34 613 09 14 94 </p>
+                    <p class=" ">administracion@congeladosbrisamar.es</p>
                 </div>
             </div>
             <div class="flex items-start gap-4">
-                <div class="text-end">
-                    <h2 class="text-lg font-bold">Hoja de pedido</h2>
-                    <p class="font-medium">{{ $order->formattedId }}</p>
-                    <p class="font-medium">Fecha de Entrada: {{ date('d/m/Y', strtotime($order->entry_date)) }}</p>
-                    <p class="font-medium">Fecha de Carga: {{ date('d/m/Y', strtotime($order->load_date)) }}</p>
-                    <p class="font-medium">Buyer Reference: {{ $order->buyer_reference }}</p>
+                <div class="  rounded  text-end">
+                    <h2 class="text-lg font-bold ">Hoja de pedido</h2>
+                    <p class=" font-medium"><span class="">{{ $order->formattedId }}</span></p>
+                    <p class=" font-medium">Fecha:<span class="">
+                            {{ date('d/m/Y', strtotime($order->load_date)) }}
+                        </span></p>
+                    <p class=" font-medium">Buyer Reference:{{ $order->buyer_reference }}</p>
                 </div>
                 <div class="flex flex-col items-center">
                     <div class="p-1 border rounded flex items-center justify-center bg-white">
@@ -51,68 +55,63 @@
                 </div>
             </div>
         </div>
-
-        <!-- INFORMACIÓN DEL CLIENTE Y DIRECCIONES -->
         <div class="grid grid-cols-2 gap-4 mb-6 text-xs">
-            <div class="border rounded-lg overflow-hidden bg-gray-50">
-                <div class="font-bold mb-2 w-full p-2 bg-gray-800 border-b text-white">DIRECCIÓN DE ENVÍO</div>
-                <div class="space-y-1 p-4 pt-0">
-                    <p>{!! nl2br($order->shipping_address) !!}</p>
-                </div>
-            </div>
-            <div class="border rounded-lg overflow-hidden bg-gray-50">
-                <div class="font-bold mb-2 w-full p-2 bg-gray-800 border-b text-white">DIRECCIÓN DE FACTURACIÓN</div>
-                <div class="space-y-1 p-4 pt-0">
-                    <p>{!! nl2br($order->billing_address) !!}</p>
-                </div>
-            </div>
-        </div>
+            <div class="space-y-4">
+                <div class="border rounded-lg overflow-hidden bg-gray-50">
+                    <div class="font-bold  mb-2 w-full p-2 bg-gray-800 border-b text-white">DATOS DEL CLIENTE</div>
+                    <div class=" space-y-1 p-4 pt-0">
+                        <p><span class="font-medium">Nombre:</span> {{ $order->customer->name }}</p>
+                        <p><span class="font-medium">NIF/CIF:</span>{{ $order->customer->vat_number }}</p>
 
-        <!-- DETALLE DE PRODUCTOS -->
-        <div class="mb-6">
-            <h3 class="font-bold mb-2">DETALLE DE PRODUCTOS</h3>
-            <div class="border border-gray-300 rounded-lg overflow-hidden">
-                <table class="w-full text-xs h-full">
-                    <thead class="border-b bg-white">
-                        <tr class="bg-gray-100">
-                            <th class="p-2 font-bold text-start">Producto</th>
-                            <th class="p-2 font-bold text-start">Código GTIN</th>
-                            <th class="p-2 font-bold text-start">Lote</th>
-                            <th class="p-2 font-bold text-start">Cajas</th>
-                            <th class="p-2 font-bold text-start">Peso Neto</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($order->productsWithLotsDetails as $productLine)
-                            <tr class="{{ $loop->even ? 'bg-white' : 'bg-gray-50' }}">
-                                <td class="p-2 py-1">{{ $productLine['product']['article']['name'] }}</td>
-                                <td class="p-2 py-1">{{ $productLine['product']['boxGtin'] }}</td>
-                                <td class="p-2 py-1">
-                                    {{ count($productLine['lots']) === 1 ? $productLine['lots'][0]['lot'] : '' }}</td>
-                                <td class="p-2 py-1">{{ $productLine['product']['boxes'] }}</td>
-                                <td class="p-2 py-1">
-                                    {{ number_format($productLine['product']['netWeight'], 2, ',', '.') }} kg</td>
-                            </tr>
-                            @if (count($productLine['lots']) > 1)
-                                @foreach ($productLine['lots'] as $lot)
-                                    <tr class="{{ $loop->even ? 'bg-white' : 'bg-gray-50' }} text-[10px]">
-                                        <td class="p-2 py-1"></td>
-                                        <td class="text-md text-end">↪︎</td>
-                                        <td class="p-2 py-1">{{ $lot['lot'] }}</td>
-                                        <td class="p-2 py-1">{{ $lot['boxes'] }}</td>
-                                        <td class="p-2 py-1">{{ number_format($lot['netWeight'], 2, ',', '.') }} kg
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            @endif
-                        @endforeach
-                    </tbody>
-                </table>
+                        <p class="font-medium mt-2">Correos electrónicos:</p>
+                        <ul class="list-disc pl-5">
+                            {{-- $order->emailsArray --}}
+                            @foreach ($order->emailsArray as $email)
+                            <li>{{ $email }}</li>
+                            @endforeach
+                            {{-- $order->ccEmailsArray --}}
+                            @foreach ($order->ccEmailsArray as $email)
+                            <li>{{ $email }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+                <div class="border rounded-lg overflow-hidden bg-gray-50">
+                    <div class="font-bold  mb-2 w-full p-2 bg-gray-800 border-b text-white">DATOS DE TRANSPORTE</div>
+                    <div class=" space-y-1 p-4 pt-0">
+                        <p><span class="font-medium">Empresa:</span> {{ $order->transport->name }}</p>
+                        <p class="font-medium mt-2">Correos electrónicos:</p>
+                        <ul class="list-disc pl-5">
+                            {{-- $order->transport->emailsArray y $order->ccEmailsArray --}}
+                            @foreach ($order->transport->emailsArray as $email)
+                            <li>{{ $email }}</li>
+                            @endforeach
+                            @foreach ($order->ccEmailsArray as $email)
+                            <li>{{ $email }}</li>
+                            @endforeach
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="border rounded-lg overflow-hidden bg-gray-50">
+                <div class="font-bold  mb-2 w-full p-2 bg-gray-800 border-b text-white">DIRECCIONES</div>
+                <div class=" space-y-1 p-4 pt-0">
+                    <h3 class="font-bold  mb-2">DIRECCIÓN DE FACTURACIÓN</h3>
+                    <p class="">
+                        {!! nl2br($order->billing_address) !!}
+                    </p>
+                    <hr class="my-4 border-dashed border-slate-300" />
+                    <h3 class="font-bold  mb-2">DIRECCIÓN DE ENVÍO</h3>
+                    <p class="">
+                        {!! nl2br($order->shipping_address) !!}
+                    </p>
+                </div>
             </div>
         </div>
 
         <!-- SECCIÓN COMPACTADA: INCOTERM, FORMA DE PAGO Y PALETS -->
-        <div class="border p-4 rounded-lg bg-gray-50 mb-6">
+        <div class="border p-4 rounded-lg bg-gray-50 mb-6 text-[10px]">
             <div class="grid grid-cols-3 gap-4">
                 <div>
                     <p class="font-bold">FORMA DE PAGO</p>
@@ -131,7 +130,7 @@
         </div>
 
         <!-- OBSERVACIONES DIVIDIDAS EN TARJETAS -->
-        <div class="grid grid-cols-3 gap-4 text-xs">
+        <div class="grid grid-cols-3 gap-4 text-[10px]">
             <div class="border p-4 rounded-lg bg-gray-50">
                 <h3 class="font-bold mb-2">OBSERVACIONES PRODUCCIÓN</h3>
                 <p>{{ $order->production_notes }}</p>
@@ -143,6 +142,93 @@
             <div class="border p-4 rounded-lg bg-gray-50">
                 <h3 class="font-bold mb-2">OBSERVACIONES TRANSPORTE</h3>
                 <p>{{ $order->transportation_notes }}</p>
+            </div>
+        </div>
+
+        <div class=" mb-6 ">
+            <h3 class="font-bold mb-2">DETALLE DE PRODUCTOS</h3>
+            <div class="border border-gray-300 rounded-lg overflow-hidden">
+                <table class="w-full text-xs h-full ">
+                    <thead class="border-b bg-white ">
+                        <tr class="bg-gray-100">
+                            <th class="p-2 font-bold text-start">Producto</th>
+                            <th class="p-2 font-bold text-start">Código GTIN</th>
+                            <th class="p-2 font-bold text-start">Lote</th>
+                            <th class="p-2 font-bold text-start">Cajas</th>
+                            <th class="p-2 font-bold text-start">Peso Neto</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                        $rowIndex = 0; // Controlador manual para alternar colores
+                        @endphp
+
+                        @foreach ($order->productsWithLotsDetails as $productLine)
+                        @php
+                        $rowClass = $rowIndex % 2 === 0 ? 'bg-white' : 'bg-gray-50';
+                        $rowIndex++; // Incrementamos el contador de filas
+                        @endphp
+
+                        <!-- Fila principal del producto -->
+                        <tr class="{{ $rowClass }}">
+                            <td class="p-2 py-1">{{ $productLine['product']['article']['name'] }}</td>
+                            <td class="p-2 py-1">{{ $productLine['product']['boxGtin'] }}</td>
+                            <td class="p-2 py-1">
+                                {{ count($productLine['lots']) === 1 ? $productLine['lots'][0]['lot'] : '' }}
+                            </td>
+                            <td class="p-2 py-1">{{ $productLine['product']['boxes'] }}</td>
+                            <td class="p-2 py-1">
+                                {{ number_format($productLine['product']['netWeight'], 2, ',', '.') }} kg
+                            </td>
+                        </tr>
+
+                        <!-- Fila con información de la especie -->
+                        @php
+                        $rowClass = $rowIndex % 2 === 0 ? 'bg-white' : 'bg-gray-50';
+                        $rowIndex++;
+                        @endphp
+                        <tr class="{{ $rowClass }}">
+                            <td class="pl-5 p-2 py-1 text-[10px]" colspan="5">
+                                <i>
+                                    {{ $productLine['product']['species']['name'] }}
+                                    `{{ $productLine['product']['species']['scientificName'] }} -
+                                    {{ $productLine['product']['species']['fao'] }}`
+                                    - {{ $productLine['product']['fishingGear'] }} /
+                                    {{ $productLine['product']['captureZone'] }}
+                                </i>
+                            </td>
+                        </tr>
+
+                        <!-- Si hay más de un lote, se imprimen en filas separadas -->
+                        @if (count($productLine['lots']) > 1)
+                        @foreach ($productLine['lots'] as $lot)
+                        @php
+                        $rowClass = $rowIndex % 2 === 0 ? 'bg-white' : 'bg-gray-50';
+                        $rowIndex++;
+                        @endphp
+                        <tr class="{{ $rowClass }} text-[10px]">
+                            <td class="p-2 py-1"></td>
+                            <td class="text-md text-end">↪︎</td>
+                            <td class="p-2 py-1">{{ $lot['lot'] }}</td>
+                            <td class="p-2 py-1">{{ $lot['boxes'] }}</td>
+                            <td class="p-2 py-1">{{ number_format($lot['netWeight'], 2, ',', '.') }} kg
+                            </td>
+                        </tr>
+                        @endforeach
+                        @endif
+                        @endforeach
+
+                        <tr className='font-bold '>
+                            <td class="p-2 py-1 border-t bg-gray-100"></td>
+                            <td class="p-2 py-1 border-t bg-gray-100"></td>
+                            <td class="p-2 py-1 border-t bg-gray-100">Total</td>
+                            <td class="p-2 py-1 border-t bg-gray-100">{{ $order->totalBoxes }} </td>
+                            <td class="p-2 py-1 border-t bg-gray-100">
+                                {{ number_format($order->totalNetWeight, 2, ',', '.') }} kg
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
 
