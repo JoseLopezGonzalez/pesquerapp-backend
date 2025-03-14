4746ed9d0525024d0bef44870489c10723ce74c6
Jose-PCBRISAMAR <tijito4@gmail.com>
2025-03-14 17:19:47 +0100

    Agregando gestion de envio de documentación

--- resources/views/emails/orders/commercial.blade.php
+++ resources/views/emails/orders/commercial.blade.php
@@ -1,4 +1,4 @@
-<x-mail::message>
+<!-- <x-mail::message>
 
     # {{ $order->customer->name }}
 
@@ -34,4 +34,36 @@
     Saludos cordiales,
     **Departamento de Operaciones**
 
+</x-mail::message> -->
+
+<x-mail::message>
+
+    # {{ $order->customer->name }}
+
+    <br>
+
+    **ES -** Su pedido con número **{{$order->formattedId}}** ha sido enviado. En breve recibirá su factura.
+
+
+
+    **IT -** Il suo ordine con il numero **{{$order->formattedId}}** è stato spedito. Riceverà presto la sua fattura.
+
+
+
+    **EN -** Your order with number **{{$order->formattedId}}** has been shipped. You will receive your invoice shortly.
+
+
+
+    **FR -** Votre commande avec le numéro **{{$order->formattedId}}** a été expédiée. Vous recevrez bientôt votre
+    facture.
+
+
+
+    **PT -** Seu pedido com o número **{{$order->formattedId}}** foi enviado. Você receberá sua fatura em breve.
+
+    <br>
+
+
+
+    Saludos / Saluti / Best regards / Cordialement / Atenciosamente.
 </x-mail::message>
