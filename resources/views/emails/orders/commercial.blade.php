afe435fbbcf7ded4606a1a575fa15e836efb9a00
Jose-PCBRISAMAR <tijito4@gmail.com>
2025-03-14 16:54:46 +0100

    Agregando gestion de envio de documentación

--- resources/views/emails/orders/commercial.blade.php
+++ resources/views/emails/orders/commercial.blade.php
@@ -1,23 +1,35 @@
 <x-mail::message>
 
-    # Pedido {{ $order->formattedId }} - {{ $order->customer->name }}
+    # {{ $order->customer->name }}
+
+    <br>
+
+    **Pedido Nº:** **{{ $order->formattedId }}**
+    **Fecha de Carga:** {{ date('d/m/Y', strtotime($order->load_date)) }}
 
     <br>
 
     Estimado/a equipo comercial,
 
     Adjuntamos los documentos correspondientes al pedido **#{{ $order->formattedId }}** de
-    **{{ $order->customer->name }}**, con fecha de carga **{{ date('d/m/Y', strtotime($order->load_date)) }}**.
+    **{{ $order->customer->name }}**.
 
     <br>
 
-    Documentos incluidos:
+    **Documentos incluidos:**
     - Nota de Carga
     - Packing List
 
     <br>
 
-    Si necesitan más información, contacten con el equipo de operaciones.
+    Por favor, revisen la documentación adjunta para asegurarse de que toda la información esté correcta.
+
+    <br>
+
+    Si necesitan más información, pueden contactar directamente con el equipo de operaciones a
+    [comercial@empresa.com](mailto:comercial@empresa.com).
+
+    <br>
 
     Saludos cordiales,
     **Departamento de Operaciones**
