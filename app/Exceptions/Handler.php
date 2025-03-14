a837659c571304ef3cee3609de714ddd2b3bd139
Jose-PCBRISAMAR <tijito4@gmail.com>
2025-03-14 15:42:44 +0100

    Agregando gestion de envio de documentación

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -16,21 +16,21 @@ class Handler extends ExceptionHandler
      *
      * @var array<int, string>
      */
-    protected $dontFlash = [
+    /* protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
-    ];
+    ]; */
 
     /**
      * Register the exception handling callbacks for the application.
      */
-    public function register(): void
+    /* public function register(): void
     {
         $this->reportable(function (Throwable $e) {
             //
         });
-    }
+    } */
 
     /* Render */
     /* public function render($request, Throwable $exception)
