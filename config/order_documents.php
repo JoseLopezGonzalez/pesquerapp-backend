a96b0566951331ae77427b0bc4d74ccbe32a5100
Jose Laptop Unir <tijito4@gmail.com>
2025-03-15 20:50:57 +0100

    ACtualizando

--- config/order_documents.php
+++ config/order_documents.php
@@ -2,13 +2,13 @@
 
 return [
     'documents' => [
-        'loading_note' => [
+        'loading-note' => [
             'document_name' => 'Nota de Carga',
             'view_path' => 'pdf.v2.orders.loading_note',
             'subject_template' => 'Nota de Carga - Pedido #{order_id}',
             'body_template' => 'emails.orders.generic', // ✅ Cambiado a genérico
         ],
-        'packing_list' => [
+        'packing-list' => [
             'document_name' => 'Packing List',
             'view_path' => 'pdf.v2.orders.order_packing_list',
             'subject_template' => 'Packing List - Pedido #{order_id}',
@@ -28,8 +28,4 @@ return [
         'transporte' => ['cmr', 'packing_list'],
     ],
 
-
-    'recipients' => [
-        // Otros destinatarios personalizados si quieres añadir después
-    ],
 ];
