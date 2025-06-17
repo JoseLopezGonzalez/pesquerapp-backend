ac0a5784be365e5f5465d0f1d91c709744eff4d9
Jose-PCBRISAMAR <tijito4@gmail.com>
2025-06-17 18:31:42 +0200

    Actualizando

--- app/Http/Resources/v2/ProductResource.php
+++ app/Http/Resources/v2/ProductResource.php
@@ -14,14 +14,7 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return array_merge($this->article->toArrayAssoc() , [
-            'species' => $this->species->toArrayAssoc(),
-            'captureZone' => $this->captureZone->toArrayAssoc(),
-            'articleGtin' => $this->article_gtin,
-            'boxGtin' => $this->box_gtin,
-            'palletGtin' => $this->pallet_gtin,
-            'fixedWeight' => $this->fixed_weight,
-        ]);
+        return $this->toArrayAssoc();
     }
 
 
