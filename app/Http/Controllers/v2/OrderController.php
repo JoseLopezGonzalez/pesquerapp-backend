0fc7afbd7c7ced8b7372199c9d0cadf3e4c8d5d5
Jose-PCBRISAMAR <tijito4@gmail.com>
2025-06-19 13:39:00 +0200

    Actualizando

--- app/Http/Controllers/v2/OrderController.php
+++ app/Http/Controllers/v2/OrderController.php
@@ -285,7 +285,7 @@ class OrderController extends Controller
             'truckPlate' => 'sometimes|nullable|string',
             'trailerPlate' => 'sometimes|nullable|string',
 
-            'temperature' => 'sometimes|integer',
+            'temperature' => 'sometimes|nullable|integer',
         ]);
 
         $order = Order::findOrFail($id);
