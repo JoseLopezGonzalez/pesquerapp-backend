15beae6246e5112cceebd19f2e27e8c3d99416c6
Jose-Laptop_Eli <tijito4@gmail.com>
2025-07-19 18:35:35 +0200

    Actualizando

--- app/Models/Transport.php
+++ app/Models/Transport.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use App\Traits\UsesTenantConnection;
+/* use App\Traits\UsesTenantConnection; */
 
+use App\Traits\UsesTenantConnection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
